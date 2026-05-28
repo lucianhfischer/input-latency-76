d03820248023df1bccfe5126fabaa85637d45de9
Yuki Tanaka <lucianhfischer@onet.pl>
2026-05-29 04:09:19 +0900

    Update implement game logger utility function

--- logger.py
+++ logger.py
@@ -1,32 +1,30 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name, log_file='app.log', max_bytes=1024*1024, backup_count=5):
+class GameLogger:
+    def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler = logging.FileHandler(f'{name}.log')
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
-    def debug(self, message):
-        self.logger.debug(message)
-
-    def info(self, message):
+    def log_info(self, message):
         self.logger.info(message)
 
-    def warning(self, message):
+    def log_warning(self, message):
         self.logger.warning(message)
 
-    def error(self, message):
+    def log_error(self, message):
         self.logger.error(message)
 
-    def critical(self, message):
-        self.logger.critical(message)
+    def log_debug(self, message):
+        self.logger.debug(message)
 
-# Example usage:
 if __name__ == '__main__':
-    log = Logger('game_logger')
-    log.info('This is an info message')
-    log.error('This is an error message')
+    game_logger = GameLogger('game_events')
+    game_logger.log_info('Game initialized successfully.')
+    game_logger.log_warning('Low memory warning!')
+    game_logger.log_error('Failed to load resource.')
+    game_logger.log_debug('Debugging game mechanics.')
